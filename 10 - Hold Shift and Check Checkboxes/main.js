fc5f971197c6c21e116f04cc20b6320fe79cef7e
Trugamr <6trugamr9@gmail.com>
2019-04-22 21:43:44 +0530

    hold shift and check checkboxes

---
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -0,0 +1,25 @@
+let checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+    let inBetween = false;
+    // check if shift key down and check if they are checking it
+    if(e.shiftKey && this.checked) {
+        // loop over every single checkbox
+        checkboxes.forEach(checkbox => {
+            if(checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if(inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', handleCheck);
+})
